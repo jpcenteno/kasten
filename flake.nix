37258aa1e7cffffbba160151a4f16159702a2d46
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-01-24 10:49:34 -0300

    chore: Add Node and PNPM to the devShell

--- flake.nix
+++ flake.nix
@@ -8,9 +8,14 @@
     inputs.flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import (inputs.nixpkgs) { inherit system; });
+        nodejs = pkgs.nodejs_23;
+        pnpm = pkgs.pnpm.override { inherit nodejs; };
       in {
         devShell = pkgs.mkShell {
-          buildInputs = [];
+          buildInputs = [
+            nodejs
+            pnpm
+          ];
         };
       }
     );
