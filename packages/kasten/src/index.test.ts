407027e03b3dee46cf0af3ed39eba6e11f24cc88
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-01-25 00:34:36 -0300

    feat: Delete the greet function

--- packages/kasten/src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { greet } from "./index";
-
-describe("greet", () => {
-  it("should return the correct greeting message", () => {
-    expect(greet()).toBe("Hello from Kasten!");
-  });
-});
