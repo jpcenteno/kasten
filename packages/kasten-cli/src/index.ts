9a9d34efa94be93aca7174a0704adc27e80cad4a
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-01-26 08:03:20 -0300

    feat(cli): Implement subcommand `note new`

--- packages/kasten-cli/src/index.ts
+++ packages/kasten-cli/src/index.ts
@@ -1,3 +1,21 @@
-import { greet } from "kasten";
+import { Command } from "commander";
+import { Zettelkasten } from "kasten";
 
-console.log(greet());
+const program = new Command("kasten");
+
+const noteSubcomamnd = program.command("note");
+
+noteSubcomamnd
+  .command("new")
+  .requiredOption(
+    "-d, --directory <directory>",
+    "Path to the Zettelkasten directory",
+  )
+  .requiredOption("-t, --title <title>", "Note title")
+  .action((args: { directory: string; title: string }) => {
+    const zk = new Zettelkasten(args.directory);
+    const fileName = zk.newNote({ title: args.title, content: "some content" });
+    console.log(zk.getFullPath(fileName));
+  });
+
+program.parse();
