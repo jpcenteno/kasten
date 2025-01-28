2e5791afd9a1bb9d755c5bca2610a4406828957e
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-01-28 03:06:57 -0300

    refactor(cli): Split subcommands into modules

--- packages/kasten-cli/src/index.ts
+++ packages/kasten-cli/src/index.ts
@@ -1,35 +1,4 @@
 import { Command } from "commander";
-import { Zettelkasten } from "kasten";
+import { command as noteCommand } from "./commands/note";
 
-const program = new Command("kasten");
-
-const noteSubcomamnd = program.command("note");
-
-noteSubcomamnd
-  .command("new")
-  .requiredOption(
-    "-d, --directory <directory>",
-    "Path to the Zettelkasten directory",
-  )
-  .requiredOption("-t, --title <title>", "Note title")
-  .action((args: { directory: string; title: string }) => {
-    const zk = new Zettelkasten(args.directory);
-    const fileName = zk.newNote({ title: args.title, content: "some content" });
-    console.log(zk.getFullPath(fileName));
-  });
-
-noteSubcomamnd
-  .command("list")
-  .requiredOption(
-    "-d, --directory <directory>",
-    "Path to the Zettelkasten directory",
-  )
-  .action((args: { directory: string; title: string }) => {
-    const zk = new Zettelkasten(args.directory);
-    const notes = zk.listNotes();
-    notes.forEach(({ id, title }) => {
-      console.log(`${id}\t${title}`);
-    });
-  });
-
-program.parse();
+new Command("kasten").addCommand(noteCommand).parse();
