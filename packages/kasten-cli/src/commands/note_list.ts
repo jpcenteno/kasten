bed3a60591c55878e2df25b74ff633b06bc565d4
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-01-28 11:07:17 -0300

    feat(cli): JSON output for `note list`

--- packages/kasten-cli/src/commands/note_list.ts
+++ packages/kasten-cli/src/commands/note_list.ts
@@ -3,18 +3,32 @@ import { Zettelkasten } from "kasten";
 
 export const command = new Command("list")
   .option("-d, --directory <directory>", "Path to the Zettelkasten directory")
-  .action((args: { directory: string | undefined; title: string }) => {
-    const directory = args.directory ?? process.env.KASTEN_DIR;
+  .option("-j, --json", "Use JSON output")
+  .action(
+    (args: { directory: string | undefined; title: string; json: boolean }) => {
+      const directory = args.directory ?? process.env.KASTEN_DIR;
 
-    if (!directory || directory.trim() === "") {
-      throw new Error(
-        "The directory must be specified using -d/--directory or KASTEN_DIR must be defined.",
-      );
-    }
+      if (!directory || directory.trim() === "") {
+        throw new Error(
+          "The directory must be specified using -d/--directory or KASTEN_DIR must be defined.",
+        );
+      }
 
-    const zk = new Zettelkasten(directory);
-    const notes = zk.listNotes();
-    notes.forEach(({ id, title }) => {
-      console.log(`${id}\t${title}`);
-    });
-  });
+      const zk = new Zettelkasten(directory);
+      const notes = zk.listNotes();
+
+      if (args.json) {
+        const records = notes.map((note) => ({
+          title: note.title,
+          relativePath: note.id,
+          absolutePath: zk.getFullPath(note.id),
+        }));
+
+        console.log(JSON.stringify(records));
+      } else {
+        notes.forEach(({ id, title }) => {
+          console.log(`${id}\t${title}`);
+        });
+      }
+    },
+  );
