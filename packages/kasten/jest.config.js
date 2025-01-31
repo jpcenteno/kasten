bf68d82beed4e27bced08d510946e264a59d58a8
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-01-31 16:23:13 -0300

    test(lib): Move the Jest config to packages/kasten

--- packages/kasten/jest.config.js
+++ packages/kasten/jest.config.js
@@ -2,7 +2,7 @@ const config = {
   preset: "ts-jest",
   testEnvironment: "node", // Use 'jsdom' if you're testing browser-like code
   testMatch: [
-    "**/packages/**/?(*.)+(spec|test).ts", // Look for *.test.ts or *.spec.ts files in the packages
+    "./**/?(*.)+(spec|test).ts", // Look for *.test.ts or *.spec.ts files in the packages
   ],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
