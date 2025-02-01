10bb1d4c3c5cd51d776a57017b5b5b6b607ee813
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-01-31 21:32:18 -0300

    refactor(lib): Remove unused entities.ts module

--- packages/kasten/src/entities.ts
+++
@@ -1,9 +0,0 @@
-export type ZettelId = string & { readonly __brand: unique symbol };
-
-export function intoZettelId(value: string): ZettelId {
-  if (value === "") {
-    throw new Error("Id cannot be empty.");
-  }
-
-  return value as ZettelId;
-}
