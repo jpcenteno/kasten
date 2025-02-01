be988d356de5b5deb1a47b01893cce07e1f3a167
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-02-01 00:03:07 -0300

    test(cli): Implement (failing) tests for `note list` when there is a corrupted note in the directory

--- packages/kasten-cli/src/__test__/test_data.ts
+++ packages/kasten-cli/src/__test__/test_data.ts
@@ -1,3 +1,6 @@
+import path from "path";
+import fs from "fs";
+
 export interface TestNote {
   relativePath: string;
   txt: string;
@@ -16,3 +19,23 @@ export const testNotes: TestNote[] = [
     title: "Title 2",
   },
 ];
+
+/**
+ * Writes a "good" and a "corrupted" note to the directory.
+ *
+ * The "good" note should not have any issue with the parser while the
+ * "corrupted" note should trigger a parser error.
+ */
+export async function writeGoodNoteAndCorruptedNote(directory: string) {
+  const noteData = [
+    { name: "good.mdx", txt: "---\ntitle: Some title\n---\n" },
+    { name: "bad.mdx", txt: "---\n---\n" },
+  ];
+
+  const promises = noteData.map(({ name, txt }) => {
+    const absolutePath = path.resolve(directory, name);
+    return fs.promises.writeFile(absolutePath, txt);
+  });
+
+  Promise.all(promises);
+}
